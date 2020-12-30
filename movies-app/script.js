f3314fe695fecfd8e4f4649d5d3d6b5427606f8f
josilob <bjosilo@gmail.com>
2020-12-29 20:43:25 -0500

    api search call

--- movies-app/script.js
+++ movies-app/script.js
@@ -1,8 +1,11 @@
 const API_URL =
 	'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=87bb6c1a1b5662d9698dca39980c3a88&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_URL =
+const SEARCH_API =
 	'https://api.themoviedb.org/3/search/movie?api_key=87bb6c1a1b5662d9698dca39980c3a88&query="';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
 //get initial movies
 getMovies(API_URL);
@@ -12,3 +15,15 @@ async function getMovies(url) {
 	const data = await res.json();
 	console.log(data.results);
 }
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const searchTerm = search.value;
+	if (searchTerm && searchTerm !== '') {
+		getMovies(SEARCH_API + searchTerm);
+		search.value = '';
+	} else {
+		window.location.reload(); // refresh page - reload if nothing is being searched
+	}
+});
