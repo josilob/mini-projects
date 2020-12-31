89d75592bdf65810a55560957524b01fe27f7c64
josilob <bjosilo@gmail.com>
2020-12-30 23:17:27 -0500

    full date and time with events and theme switcher

--- theme-clock/script.js
+++ theme-clock/script.js
@@ -39,3 +39,49 @@ toggle.addEventListener('click', (e) => {
 		e.target.innerHTML = 'Light mode';
 	}
 });
+
+// map input numbers to output numbers
+// in this case hours and minutes to degrees
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+function setTime() {
+	const time = new Date();
+	const month = time.getMonth();
+	const day = time.getDay();
+	const date = time.getDate();
+	const hours = time.getHours();
+	const minutes = time.getMinutes();
+	const seconds = time.getSeconds();
+	const hoursForClock = hours % 12;
+	const ampm = hours >= 12 ? ' PM' : ' AM';
+
+	hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+		hoursForClock,
+		0,
+		11,
+		0,
+		360
+	)}deg)`;
+	minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+		minutes,
+		0,
+		59,
+		0,
+		360
+	)}deg)`;
+	secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+		seconds,
+		0,
+		59,
+		0,
+		360
+	)}deg)`;
+	timeEl.innerHTML = `${hoursForClock}:${
+		minutes < 10 ? `0${minutes}: minutes` : minutes
+	}${ampm}`;
+	dateEl.innerHTML = `${days[day]}, ${months[month]}, <span class='circle'>${date}</span>`;
+}
+
+setInterval(setTime, 1000);
