4d48e69ac83c34222b9855a4934cb24253500334
josilob <bjosilo@gmail.com>
2021-01-17 21:44:25 -0500

    game over

--- doodle-jump/script.js
+++ doodle-jump/script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const game = document.querySelector('.game');
 	const jumper = document.createElement('div');
-	let jumperBottomSpace = 150;
+	let jumperBottomSpace = 250;
 	let jumperLeftSpace = 50;
 	let platformsCount = 5;
+	let platforms = [];
+	let upTimerId;
+	let downTimerId;
 
 	let isGameOver = false;
 	function createJumper() {
@@ -33,13 +36,57 @@ document.addEventListener('DOMContentLoaded', () => {
 			let platformGap = 600 / platformsCount;
 			let newPlatformBottom = 100 + i * platformGap;
 			let newPlatform = new Platform(newPlatformBottom);
+			platforms.push(newPlatform);
+			// console.log(newPlatform);
 		}
+	}
+
+	function movePlatforms() {
+		if (jumperBottomSpace > 200) {
+			platforms.forEach((platform) => {
+				platform.bottom -= 4;
+				let visual = platform.visual;
+				visual.style.bottom = platform.bottom + 'px';
+			});
+		}
+	}
+
+	function jump() {
+		clearInterval(downTimerId);
+		upTimerId = setInterval(function () {
+			jumperBottomSpace += 20;
+			jumper.style.bottom = jumperBottomSpace + 'px';
+			if (jumperBottomSpace > 350) {
+				fall();
+			}
+		}, 30);
+	}
+
+	function fall() {
+		clearInterval(upTimerId);
+		downTimerId = setInterval(function () {
+			jumperBottomSpace -= 5;
+			jumper.style.bottom = jumperBottomSpace + 'px';
+			if (jumperBottomSpace <= 0) {
+				gameOver();
+			}
+		}, 30);
+	}
+
+	function gameOver() {
+		console.log('Game Over');
+		isGameOver = true;
+		clearInterval(upTimerId);
+		clearInterval(downTimerId);
 	}
 
 	function start() {
 		if (!isGameOver) {
 			createJumper();
 			createPlatforms();
+			setInterval(movePlatforms, 30);
+			jump();
+			fall();
 		}
 	}
 
