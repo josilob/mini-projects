404c15a3c23995921aac8c883fbb3bc6e07146bf
josilob <bjosilo@gmail.com>
2021-01-20 22:23:29 -0500

    winner logic, stop further game

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -18,6 +18,7 @@ window.onload = () => {
 		allBox[i].setAttribute('onclick', 'clickedBox(this)');
 	}
 };
+
 selectBtnX.onclick = () => {
 	selectBox.classList.add('hide'); //hide select box
 	playBoard.classList.add('show'); //show the playboard section
@@ -32,6 +33,7 @@ selectBtnO.onclick = () => {
 let playerXIcon = 'fas fa-times';
 let playerOIcon = 'fas fa-circle';
 let playerSign = 'X'; // we guess this
+let runBot = true;
 
 // user's click fn
 function clickedBox(element) {
@@ -46,38 +48,76 @@ function clickedBox(element) {
 		players.classList.add('active');
 		element.setAttribute('id', playerSign);
 	}
+	selectWinner();
+	playBoard.style.pointerEvents = 'none'; // once player makes a move, he can't check box until his move again
 	element.style.pointerEvents = 'none'; // once selected, that box can't be selected again
 	let randomDelayTime = (Math.random() * 1000 + 200).toFixed(); //generate random bot delay
 	setTimeout(() => {
-		bot();
+		bot(runBot);
 	}, randomDelayTime);
 }
 
 // bot click fn
-function bot() {
-	//first change playerSign, so if user has X, than bot will be O
-	playerSign = 'O';
-	let array = []; // empty array to store inselected boxes in it
-	for (let i = 0; i < allBox.length; i++) {
-		//if there are no span elements
-		if (allBox[i].childElementCount == 0) {
-			// insert unselected ones
-			array.push(i);
+function bot(runBot) {
+	if (runBot) {
+		//first change playerSign, so if user has X, than bot will be O
+		playerSign = 'O';
+		let array = []; // empty array to store inselected boxes in it
+		for (let i = 0; i < allBox.length; i++) {
+			//if there are no span elements
+			if (allBox[i].childElementCount == 0) {
+				// insert unselected ones
+				array.push(i);
+			}
 		}
-	}
-	let randomBox = array[Math.floor(Math.random() * array.length)];
-	if (array.length > 0) {
-		if (players.classList.contains('player')) {
-			allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
-			players.classList.remove('active');
-			//if user is O than the box id value will be X
-			playerSign = 'X';
-			allBox[randomBox].setAttribute('id', playerSign);
-		} else {
-			allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
-			players.classList.remove('active');
-			allBox[randomBox].setAttribute('id', playerSign);
+
+		let randomBox = array[Math.floor(Math.random() * array.length)];
+		if (array.length > 0) {
+			if (players.classList.contains('player')) {
+				allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
+				players.classList.remove('active');
+				//if user is O than the box id value will be X
+				playerSign = 'X';
+				allBox[randomBox].setAttribute('id', playerSign);
+			} else {
+				allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
+				players.classList.remove('active');
+				allBox[randomBox].setAttribute('id', playerSign);
+			}
+			selectWinner();
 		}
+		allBox[randomBox].style.pointerEvents = 'none'; // so we can't click over the field that bot has marked
+		playBoard.style.pointerEvents = 'auto';
+		playerSign = 'X';
 	}
-	allBox[randomBox].style.pointerEvents = 'none'; // so we can't click over the field that bot has marked
+}
+
+// winner logic
+
+function getID(idname) {
+	return document.querySelector('.box' + idname).id; // returning id name
+}
+
+function checkIDs(val1, val2, val3, sign) {
+	if (getID(val1) == sign && getID(val2) == sign && getID(val3) == sign) {
+		return true;
+	}
+}
+
+function selectWinner() {
+	// if one of the combinations is matches, select winner
+	if (
+		checkIDs(1, 2, 3, playerSign) ||
+		checkIDs(4, 5, 6, playerSign) ||
+		checkIDs(7, 8, 9, playerSign) ||
+		checkIDs(1, 4, 7, playerSign) ||
+		checkIDs(2, 5, 8, playerSign) ||
+		checkIDs(3, 6, 9, playerSign) ||
+		checkIDs(1, 5, 9, playerSign) ||
+		checkIDs(3, 5, 7, playerSign)
+	) {
+		console.log(playerSign + ' ' + 'is the winner');
+		runBot = false;
+		bot(runBot);
+	}
 }
