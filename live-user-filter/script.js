a9d5ebd28da0f9a26172ae24ae795ad48167a931
josilob <bjosilo@gmail.com>
2021-01-31 23:48:46 -0500

    minor tweaks

--- live-user-filter/script.js
+++ live-user-filter/script.js
@@ -4,15 +4,15 @@ const listItems = [];
 
 getData();
 
-filter.addEventListener('input', (e) => {
-	filterData(e.target.value);
-});
+filter.addEventListener('input', (e) => filterData(e.target.value));
 
 async function getData() {
 	const res = await fetch('https://randomuser.me/api?results=50');
+
 	const { results } = await res.json();
-	//clear results
-	results.innerHTML = '';
+
+	// Clear result
+	result.innerHTML = '';
 
 	results.forEach((user) => {
 		const li = document.createElement('li');
@@ -20,11 +20,11 @@ async function getData() {
 		listItems.push(li);
 
 		li.innerHTML = `
-        <img src="${user.picture.large}" alt="${user.name.first}"
-        <div class='user-info'>
-            <h4>${user.name.first} ${user.name.last}</h4>
-            <p>${user.location.city}, ${user.location.country}</p>
-        </div>
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
         `;
 
 		result.appendChild(li);
