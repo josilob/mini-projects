8427acf1a1511242f73ddac43b0c7625e8d5ac62
josilob <bjosilo@gmail.com>
2021-02-13 00:02:08 -0500

    done

--- mouse-move-shadow/script.js
+++ mouse-move-shadow/script.js
@@ -1,11 +1,25 @@
 const hero = document.querySelector('.hero');
-const text = document.querySelector('h1');
+const text = hero.querySelector('h1');
+const walk = 100; // 500px
 
 function shadow(e) {
 	const { offsetWidth: width, offsetHeight: height } = hero;
 	let { offsetX: x, offsetY: y } = e;
 
-	console.log(x, y);
+	if (this !== e.target) {
+		x = x + e.target.offsetLeft;
+		y = y + e.target.offsetTop;
+	}
+
+	const xWalk = Math.round((x / width) * walk - walk / 2);
+	const yWalk = Math.round((y / height) * walk - walk / 2);
+
+	text.style.textShadow = `
+        ${xWalk}px ${yWalk}px 0 rgba(227, 47, 2,0.7),
+        ${xWalk}px ${yWalk * -1}px 0 rgba(227, 96, 2,0.7),
+        ${xWalk * -1}px ${yWalk}px 0 rgba(245, 200, 76,0.7),
+        ${xWalk * -1}px ${yWalk * -1}px 0 rgba(245, 222, 76,0.7)
+    `;
 }
 
 hero.addEventListener('mousemove', shadow);
