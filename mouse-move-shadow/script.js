74abac3fdff7c333cfe2f8cf32288051f4d471e5
josilob <bjosilo@gmail.com>
2021-02-17 23:41:35 -0500

    project init

--- mouse-move-shadow/script.js
+++ mouse-move-shadow/script.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
-const walk = 100; // 500px
+const walk = 100; //px
 
 function shadow(e) {
 	const { offsetWidth: width, offsetHeight: height } = hero;
