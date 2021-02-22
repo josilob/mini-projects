5b5df77dc1aef0c8abb2f0c1674640f4c67a758e
josilob <bjosilo@gmail.com>
2021-02-22 00:20:57 -0500

    variables

--- mouse-move-shadow/script.js
+++ mouse-move-shadow/script.js
@@ -1,9 +1,9 @@
-const hero = document.querySelector('.hero');
-const text = hero.querySelector('h1');
-const walk = 100; //px
+const box = document.querySelector('.box');
+const text = box.querySelector('h1');
+const range = 100; //px
 
 function shadow(e) {
-	const { offsetWidth: width, offsetHeight: height } = hero;
+	const { offsetWidth: width, offsetHeight: height } = box;
 	let { offsetX: x, offsetY: y } = e;
 
 	if (this !== e.target) {
@@ -11,15 +11,15 @@ function shadow(e) {
 		y = y + e.target.offsetTop;
 	}
 
-	const xWalk = Math.round((x / width) * walk - walk / 2);
-	const yWalk = Math.round((y / height) * walk - walk / 2);
+	const xRange = Math.round((x / width) * range - range / 2);
+	const yRange = Math.round((y / height) * range - range / 2);
 
 	text.style.textShadow = `
-        ${xWalk}px ${yWalk}px 0 rgba(227, 47, 2,0.7),
-        ${xWalk}px ${yWalk * -1}px 0 rgba(227, 96, 2,0.7),
-        ${xWalk * -1}px ${yWalk}px 0 rgba(245, 200, 76,0.7),
-        ${xWalk * -1}px ${yWalk * -1}px 0 rgba(245, 222, 76,0.7)
+        ${xRange}px ${yRange}px 0 rgba(227, 47, 2,0.7),
+        ${xRange}px ${yRange * -1}px 0 rgba(227, 96, 2,0.7),
+        ${xRange * -1}px ${yRange}px 0 rgba(245, 200, 76,0.7),
+        ${xRange * -1}px ${yRange * -1}px 0 rgba(245, 222, 76,0.7)
     `;
 }
 
-hero.addEventListener('mousemove', shadow);
+box.addEventListener('mousemove', shadow);
